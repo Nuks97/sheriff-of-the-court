9e751bcf60496b64ae87a1180883e45fea249577
Emmanuel Sekweta <Emmanuels@atns.co.za>
2026-07-21 12:59:49 +0200

    Update notices component

--- src/components/notices.jsx
+++ src/components/notices.jsx
@@ -15,35 +15,7 @@ export const upcomingNotices = [
     contact: '061 284 0606',
   },
 
- {
-    id: 'erf-1196-seshego-zone2',
-    kind: 'Auction',
-    title: ' Sale of Immovable Property',
-    date: '2026-08-21',
-    erf: 'ERF No. 1196, Seshego Zone 2',
-    reservePrice: 400000,
-    requirements: [
-      'Proof of residence',
-      'Copy of ID',
-      'R15,000.00 refundable registration fee',
-    ],
-    contact: '061 284 0606',
-  },
-  
-{
-    id: 'erf-1196-seshego-zone3',
-    kind: 'Auction',
-    title: 'Anabella',
-    date: '2026-08-08',
-    erf: 'ERF No. 1196, Seshego Zone 3',
-    reservePrice: 400000,
-    requirements: [
-      'Proof of residence',
-      'Copy of ID',
-      'R15,000.00 refundable registration fee',
-    ],
-    contact: '061 284 0606',
-  },
+
 
 ];
 
